054afbed201b14bb658e3d7567563f9d09c5eba4
michuW93 <michal.walkowiak93@gmail.com>
2021-02-23 17:31:42 +0100

    initial commit

---
+++ src/main/java/com/example/storageRoom/StorageRoomApplication.java
@@ -0,0 +1,14 @@
+package com.example.storageRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageRoomApplication.class, args);
+        System.out.println("test Michu");
+    }
+
+}
---
+++ src/test/java/com/example/storageRoom/StorageRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storageRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
