124ac23e8cb635d4bccf9e671929038e757b6de3
mstaniucha <michal.staniucha@gmail.com>
2021-02-24 22:12:39 +0100

    Divide to two separate modules (frontend and backend) + frontend initialization

--- backend/src/main/java/com/storageroom/StorageRoomApplication.java
+++ backend/src/main/java/com/storageroom/StorageRoomApplication.java
@@ -1,4 +1,4 @@
-package com.example.storageRoom;
+package com.storageroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
