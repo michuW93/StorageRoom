9152e9dc5ebeab2019d107be836265ddd35f7682
michuW93 <michal.walkowiak93@gmail.com>
2021-04-01 18:23:48 +0200

    adding spring security and login

--- backend/src/main/java/com/storageroom/StorageRoomApplication.java
+++ backend/src/main/java/com/storageroom/StorageRoomApplication.java
@@ -2,8 +2,10 @@ package com.storageroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"security.*"})
 public class StorageRoomApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/security/controller/LoginController.java
@@ -0,0 +1,14 @@
+package security.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @PostMapping("/storage")
+    public void login(@RequestBody String name){
+
+    }
+}
