2fe039ad4a21fb3305e0e2d29f8750222757d0bf
michuW93 <michal.walkowiak93@gmail.com>
2021-04-01 18:22:34 +0200

    adding spring security and login

---
+++ backend/src/main/java/security/config/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfiguration(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .withDefaultSchema()
+                .dataSource(dataSource)
+                .withUser("test")
+                .password("{bcrypt}" + new BCryptPasswordEncoder().encode("test"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{bcrypt}" + new BCryptPasswordEncoder().encode("admin"))
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/login*").permitAll()
+                .anyRequest().authenticated();
+    }
+}
